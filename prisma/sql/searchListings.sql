0f7c7dbca141c0ffb81f5ad1181324718f71cf1b
matth <mamatthe@students.cs.ubc.ca>
2025-01-18 22:32:30 -0800

    add sql query for listings search

---
+++ prisma/sql/searchListings.sql
@@ -0,0 +1,21 @@
+SELECT
+  id,
+  title,
+  description,
+  price,
+  "createdAt",
+  "updatedAt",
+  "userId",
+  ts_rank_cd(
+    setweight(to_tsvector('english', title), 'A') ||
+    setweight(to_tsvector('english', description), 'B'),
+    plainto_tsquery('english', $1)
+  ) AS rank
+FROM
+  "Listing"
+WHERE
+  to_tsvector('english', title) @@ plainto_tsquery('english', $1) OR
+  to_tsvector('english', description) @@ plainto_tsquery('english', $1)
+ORDER BY
+  rank DESC;
+
